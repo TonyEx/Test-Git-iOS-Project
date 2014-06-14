6ef31a4d1cc261dfbf92bccf9f236e978393b104
Tony Angelo <tonyangelox@veronica.home>
2014-06-14 17:59:36 -0400

    Initial Commit

---
+++ Git Test/TEAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TEAAppDelegate.h
+//  Git Test
+//
+//  Created by Tony Angelo on 6/14/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
