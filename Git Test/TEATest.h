4f61591a4a06106deb96777529a2bcf23f3357b2
Tony Angelo <tonyangelox@veronica.home>
2014-06-15 19:26:53 -0400

    added a test class

---
+++ Git Test/TEATest.h
@@ -0,0 +1,13 @@
+//
+//  TEATest.h
+//  Git Test
+//
+//  Created by Tony Angelo on 6/15/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TEATest : NSObject
+
+@end
---
+++ Git Test/TEATest.m
@@ -0,0 +1,13 @@
+//
+//  TEATest.m
+//  Git Test
+//
+//  Created by Tony Angelo on 6/15/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import "TEATest.h"
+
+@implementation TEATest
+
+@end
